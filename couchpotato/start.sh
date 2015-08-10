96d5e8df9718a471d088ed5479769c652593abe5
Martijn Riemers <martijn@riemers.me>
2015-08-10 13:52:26 +0200

    Got rid of the submodules and just pull them on run

--- couchpotato/start.sh
+++ couchpotato/start.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/env bash
 set -e
 
+git clone https://github.com/RuudBurger/CouchPotatoServer.git /CouchPotatoServer
+
 python /CouchPotatoServer/CouchPotato.py --data_dir "/data"
--- headphones/start.sh
+++ headphones/start.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/env bash
 set -e
 
+git clone https://github.com/rembo10/headphones.git /headphones
+
 python /headphones/Headphones.py --datadir=/data --config=/data/headphones.ini
