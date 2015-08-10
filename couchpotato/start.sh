1472fcfd6c215fbc11900ed6678496866f151df2
Martijn Riemers <martijn@riemers.me>
2015-08-10 13:47:59 +0200

    Made the tests better

--- couchpotato/start.sh
+++ couchpotato/start.sh
@@ -1,6 +1,4 @@
 #!/usr/bin/env bash
 set -e
 
-git -C /CouchPotatoServer pull origin master
-
 python /CouchPotatoServer/CouchPotato.py --data_dir "/data"
--- headphones/start.sh
+++ headphones/start.sh
@@ -1,6 +1,4 @@
 #!/usr/bin/env bash
 set -e
 
-git -C /headphones pull origin master
-
 python /headphones/Headphones.py --datadir=/data --config=/data/headphones.ini
